e2b98c5ff69b1b5bfeb7f66c35cfcefe1de86a2c
eshoiza <engrjayt200@gmail.com>
2020-01-29 22:58:09 +0100

    dynamically display date function

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 const Header = () => {
   const date = new Date()
+  const hours = date.getHours();
+  let timeOfDay
+   
+  if(hours<12){
+      timeOfDay = "Morning"
+  }else{
+      if(hours>12 && hours<17){
+          timeOfDay = "Afternoon"
+      }else{
+          timeOfDay = "Night"
+      }
+  }
+
     return (
       <div>
-       <h1>It is currently about {date.getHours() % 12} o'clock</h1>
+       <h1>Good {timeOfDay}</h1>
       </div>
     );
 }
