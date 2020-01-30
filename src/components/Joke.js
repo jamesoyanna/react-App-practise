b228b7be9072e5919e9dd78b81d702e50672f75a
eshoiza <engrjayt200@gmail.com>
2020-01-30 23:25:59 +0100

    using conditional statements with props

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Joke = (props) => {
-  
     return (
       <div>
-        <h3>Question:{props.question} </h3>
+        <h3 style={{ display: props.punchline ? "block" : "none" }}>
+          Question:{props.question}
+        </h3>
         <h3>Answer:{props.punchline} </h3>
       </div>
     );
